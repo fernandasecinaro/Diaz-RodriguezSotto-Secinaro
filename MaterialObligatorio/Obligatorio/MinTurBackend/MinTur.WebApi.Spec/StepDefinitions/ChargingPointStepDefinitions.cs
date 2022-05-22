733eadb5d4e8e8d02efb380d60ac920f888bb2ea
Juan Pablo Rodriguez Sotto <juan.rodriguezsotto@gmail.com>
2022-05-22 15:31:37 -0300

    Add specflow project and charging point controller

---
+++ MaterialObligatorio/Obligatorio/MinTurBackend/MinTur.WebApi.Spec/StepDefinitions/ChargingPointStepDefinitions.cs
@@ -0,0 +1,59 @@
+using FluentAssertions;
+
+namespace MinTur.WebApi.Spec.StepDefinitions
+{
+    [Binding]
+    public sealed class ChargingPointStepDefinitions
+    {
+        private int _id;
+        private string _name;
+        private string _description;
+        private string _address;
+        private int _touristSpot;
+        private int _result;
+
+        [Given("the the id (.*)")]
+        public void GivenTheFirstNumberIs(int id)
+        {
+            _id = id;
+        }
+
+        [Given("the name (.*)")]
+        public void GivenTheNameIs(string name)
+        {
+            _name = name;
+        }
+
+        [Given("the address (.*)")]
+        public void GivenTheAddressIs(string address)
+        {
+            _address = address;
+        }
+
+        [Given("the tourist spot (.*)")]
+        public void GivenTheTouristSpotIs(int touristSpot)
+        {
+            _touristSpot = touristSpot;
+        }
+
+        [Given("the description (.*)")]
+        public void GivenTheDescriptionIs(string description)
+        {
+            _description = description;
+        }
+
+        [When("I click \"Add charging point\"")]
+        public void WhenIClickAddChargingPoint()
+        {
+            //TODO: implement act (action) logic
+
+            throw new PendingStepException();
+        }
+
+        [Then("I recieve a message showing (.*)")]
+        public void ThenTheResultShouldBe(string result)
+        {
+            _result.Should().Equals(result);
+        }
+    }
+}
